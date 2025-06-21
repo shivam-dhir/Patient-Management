59c82fc5879584a25a75f62a8045a86cc19c611a
shivam130502 <shivamdhir90@gmail.com>
2025-06-21 00:59:01 -0700

    Added Patient Repository

---
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -0,0 +1,13 @@
+package com.pm.patientservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pm.patientservice.model.Patient;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient,UUID> {
+
+}
