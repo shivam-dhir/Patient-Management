0d14fcf6da15239c77487f9a056e030defca924a
shivam130502 <shivamdhir90@gmail.com>
2025-06-21 17:12:52 -0700

    Added service and dto layer

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getAllPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patientRepository.findAll();
+    }
+
+}
