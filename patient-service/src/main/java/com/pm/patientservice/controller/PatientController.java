ac026019e69aa3d07ce3fa02f1ca271ff524e471
shivam130502 <shivamdhir90@gmail.com>
2025-06-21 17:59:04 -0700

    Added Patient Controller

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,17 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.service.PatientService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService){
+        this.patientService = patientService;
+    }
+
+}
