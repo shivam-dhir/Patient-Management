58ba37bf502c49dbd6c781953721a342bf69f290
shivam130502 <shivamdhir90@gmail.com>
2025-10-14 00:13:20 -0700

    setting delete stack flag in localstack deployment

--- infrastructure/localstack-deploy.sh
+++ infrastructure/localstack-deploy.sh
@@ -2,9 +2,12 @@
 
 set -e # Stops the script if any command fails
 
+aws --endpoint-url=http://localhost:4566 cloudformation delete-stack \
+    --stack-name patient-management
+
 # use aws cli to deploy cloud formation stack
 # stack-name refers to the name of our stack
-aws --endpoint-url=http:localhost:4566 cloudformation deploy \
+aws --endpoint-url=http://localhost:4566 cloudformation deploy \
     --stack-name patient-management \
     --template-file "./cdk.out/localstack.template.json"
 
@@ -12,5 +15,7 @@ aws --endpoint-url=http:localhost:4566 cloudformation deploy \
 #elbv2 -> elastic load balancer version 2
 # query will print out the first load balancer's DNS name that we use to access at api gateway in our stack, and return it as text
 # describe-load-balancers returns all load balancers and their addresses in our stack
-aws --endpoint-url=http:localhost:4566 elbv2 describe-load-balancers \
+aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
     --query "LoadBalancers[0].DNSName" --output text
+
+aws cloudformation describe-stack-events --stack-name patient-management
