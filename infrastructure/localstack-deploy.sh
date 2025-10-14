4ab4658d79fb426704a47970695b3b5ece982c7f
shivam130502 <shivamdhir90@gmail.com>
2025-10-13 23:49:38 -0700

    creating script for localstack/aws deployment

---
+++ infrastructure/localstack-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+set -e # Stops the script if any command fails
+
+# use aws cli to deploy cloud formation stack
+# stack-name refers to the name of our stack
+aws --endpoint-url=http:localhost:4566 cloudformation deploy \
+    --stack-name patient-management \
+    --template-file "./cdk.out/localstack.template.json"
+
+# we need an endpoint for our loadbalancer url, where we can make requests to
+#elbv2 -> elastic load balancer version 2
+# query will print out the first load balancer's DNS name that we use to access at api gateway in our stack, and return it as text
+# describe-load-balancers returns all load balancers and their addresses in our stack
+aws --endpoint-url=http:localhost:4566 elbv2 describe-load-balancers \
+    --query "LoadBalancers[0].DNSName" --output text
